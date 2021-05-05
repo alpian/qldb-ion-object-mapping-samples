7b2b1d54472d0d52ec1bd39c201bbf618ed0e3f8
Ian Davies <idda@amazon.com>
2021-05-05 00:10:05 -0700

    Add basic project structure

---
+++ Amazon.Ion.ObjectMappingSamples/Samples.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Amazon.Ion.ObjectMappingSamples
+{
+    public class Samples
+    {
+    }
+}
---
+++ Amazon.Ion.ObjectMappingSamplesTest/SamplesTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Amazon.Ion.ObjectMappingSamplesTest
+{
+    [TestClass]
+    public class SamplesTest
+    {
+        [TestMethod]
+        public void QueryReturnsUniformRows()
+        {
+        }
+    }
+}
